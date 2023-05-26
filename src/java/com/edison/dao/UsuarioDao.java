4525af7b57a642433a42d4a10701242d20258d45
Davidleo107 <104842319+Davidleo107@users.noreply.github.com>
2023-05-26 11:09:22 -0500

    Update UsuarioDao.java

--- src/java/com/edison/dao/UsuarioDao.java
+++ src/java/com/edison/dao/UsuarioDao.java
@@ -25,14 +25,17 @@ import javax.persistence.Query;
 public class UsuarioDao implements UsuarioDaoLocal {
 
     @PersistenceContext
-    private EntityManager em;
+    private EntityManager entityManager;
 
+    /**
+     * Método implementado para añadir usuarios.
+     */
     @Override
     public void addUsuario(Usuario usuario) {
         try {
             Usuario existingUser = getUsuarioByCedula(usuario.getCedula());
             if (existingUser == null) {
-                em.persist(usuario);
+                entityManager.persist(usuario);
             } else {
                 throw new Exception("El usuario con cédula " + usuario.getCedula() + " ya existe en la base de datos");
             }
@@ -41,30 +44,45 @@ public class UsuarioDao implements UsuarioDaoLocal {
         }
     }
 
+     /**
+     * Método implementado para editar usuarios.
+     */
     @Override
     public void editUsuario(Usuario usuario) {
-        em.merge(usuario);
+        entityManager.merge(usuario);
     }
 
+     /**
+     * Método implementado para eliminar usuarios.
+     */
     @Override
     public void deleteUsuario(int cedula) {
-        em.remove(getUsuario(cedula));
+        entityManager.remove(getUsuario(cedula));
     }
 
+     /**
+     * Método funcional get.
+     */
     @Override
     public Usuario getUsuario(int cedula) {
-        return em.find(Usuario.class, cedula);
+        return entityManager.find(Usuario.class, cedula);
     }
 
+     /**
+     * Método implementado para obtener todos los usuarios.
+     */
     @Override
     public List<Usuario> getAllUsuarios() {
-        return em.createNamedQuery("Usuario.getAll").getResultList();
+        return entityManager.createNamedQuery("Usuario.getAll").getResultList();
     }
     
+     /**
+     * Método implementado para obtener todos los usuarios según su cédula.
+     */
     @Override
     public List <Usuario>getUsuarioByCedulaLIST(String cedula) {
         try {
-            Query query = em.createNamedQuery("Usuario.getByCedula");
+            Query query = entityManager.createNamedQuery("Usuario.getByCedula");
             query.setParameter("cedula", cedula);
             Usuario usuario = (Usuario) query.getSingleResult();
             return query.getResultList();
@@ -73,10 +91,13 @@ public class UsuarioDao implements UsuarioDaoLocal {
         }
     }
 
+     /**
+     * Método implementado para obtener los usuarios según su nombre de usuario.
+     */
     @Override
     public Usuario getUsuarioByUsername(String nombre) {
         try {
-            Query query = em.createNamedQuery("Usuario.getByUsername");
+            Query query = entityManager.createNamedQuery("Usuario.getByUsername");
             query.setParameter("nombre", nombre);
             Usuario usuario = (Usuario) query.getSingleResult();
             return usuario;
@@ -84,12 +105,14 @@ public class UsuarioDao implements UsuarioDaoLocal {
             return null;
         }
     }
-//-----------------------------------------------------------------------------------------------
 
+    /**
+     * Método implementado para obtener los usuarios según su cédula.
+     */
     @Override
     public Usuario getUsuarioByCedula(int cedula) {
         try {
-            Query query = em.createQuery("SELECT u FROM Usuario u WHERE u.cedula = :cedula");
+            Query query = entityManager.createQuery("SELECT u FROM Usuario u WHERE u.cedula = :cedula");
             query.setParameter("cedula", cedula);
             Usuario usuario = (Usuario) query.getSingleResult();
             return usuario;
